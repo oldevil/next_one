bfc44982de1c2f67c6b96d653bce6b6297340999
oldevil <oldevil@test.com>
2020-01-08 13:22:25 +0800

    添加gunicorn静态文件支持

--- next_one/urls.py
+++ next_one/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.static import serve
+from django.conf import settings
+from django.conf.urls import url
 
 urlpatterns = [
     path('room/', include('room.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if not settings.DEBUG:
+    urlpatterns += [url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})]
