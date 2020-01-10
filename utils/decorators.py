a6bfbe4440967b5c5e3df0be942a6093026da3dc
oldevil <oldevil@test.com>
2020-01-10 11:39:34 +0800

    修改decorators, 防止request.META.get(HTTP_USER_AGENT)错误异常

--- utils/decorators.py
+++ utils/decorators.py
@@ -9,7 +9,6 @@ def logit(func):
     @wraps(func)
     def with_logging(*args, **kwargs):
         logger.info(func.__name__)
-        logger.info(args[0].META['REMOTE_ADDR'])
-        logger.info(args[0].META['HTTP_USER_AGENT'])
+        logger.info(args[0].META.get('HTTP_USER_AGENT', 'unknown'))
         return func(*args, **kwargs)
     return with_logging
