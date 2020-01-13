a81473d122d160938fb89e0d00e64157750ba915
oldevil <oldevil@test.com>
2020-01-13 18:02:53 +0800

    修改decorators格式; bug fix:room_detail, surgeon_index模版favicon; 修改settings.py，本地静态文件问题

--- next_one/settings.py
+++ next_one/settings.py
@@ -141,9 +141,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+else:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 # Logging
--- utils/decorators.py
+++ utils/decorators.py
@@ -8,8 +8,11 @@ logger = logging.getLogger('deco')
 def logit(func):
     @wraps(func)
     def with_logging(*args, **kwargs):
-        logger.info(func.__name__)
-        logger.info(args[0].META.get('REMOTE_ADDR', 'unknown'))
-        logger.info(args[0].META.get('HTTP_USER_AGENT', 'unknown'))
+        detail = {
+            'FUNCTION_NAME': func.__name__,
+            'REMOTE_ADDR': args[0].META.get('REMOTE_ADDR', 'unknown'),
+            'HTTP_USER_AGENT': args[0].META.get('HTTP_USER_AGENT', 'unknown'),
+        }
+        logger.info(detail)
         return func(*args, **kwargs)
     return with_logging
